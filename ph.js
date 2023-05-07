b4f4bb8d2582c3b743f9eeb35e010a51456ea515
Pedro Rodriguez <131567856+PHrodidriguez@users.noreply.github.com>
2023-05-06 21:34:45 -0300

    Add files via upload

---
+++ ph.js
@@ -0,0 +1,15 @@
+
+function registrar(){
+    var registra = prompt("INSIRA O CODIGO DE VERIFICAÇÃO")
+
+    if(registra == "123"){
+        return location = "insiraocodigo.html"}
+
+        else{
+            alert("CODIGO INVÁLIDO")
+        }
+}
+
+
+        
+        
